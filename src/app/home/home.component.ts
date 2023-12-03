5fd8b6c903206f24321d5ccb37aea6d2ca46fc8d
Kryko7 <61829993+Kryko7@users.noreply.github.com>
2023-12-03 20:53:22 +0530

    Finished Angular Homes Tutorial

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,43 @@
+import { Component, Inject, inject } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule,
+    HousingLocationComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  logMessage() {
+    console.log('Button clicked!');
+  }
+
+  housingLocationList: HousingLocation[] = [];
+  filteredLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
+  }
+
+  filterResults(text: string) {
+    if(!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+}
